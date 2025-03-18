cad03fbf6946ff4551655120986ac86a45f67122
Anton Mrakovcic <130923459+edukacijaSTS@users.noreply.github.com>
2025-03-18 17:37:07 +0100

    Add files via upload

---
+++ data/slide50.js
@@ -0,0 +1,7 @@
+(function(){var loadHandler=window['sl_{9123AD46-CC75-4884-8465-BFF27AAFB541}'];loadHandler&&loadHandler(49, '<div id="spr0_1a23857a"><div id="spr1_1a23857a" class="kern slide"><img id="img3_1a23857a" src="data/img5.png" width="960px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_1a23857a" class="kern slide"><div id="spr3_1a23857a"><img id="img0_1a23857a" src="data/img0.jpg" width="960" height="720" alt="Imagen F"/></div><div id="spr4_1a23857a" style="left:381.667px;top:178.5px;"><img id="img1_1a23857a" src="data/img1.gif" width="302.5" height="169.333" alt="!cid_005101c90c2d$23b054b0$a69f7457@m2mrxiuv4d0gwo"/></div><div id="spr5_1a23857a" style="left:150.75px;top:366px;"><img id="img2_1a23857a" src="data/img75.png" width="692.25" height="300" alt="POZDRAV DO SLIJEDEĆEG TJEDANA!!!!\
+\
+REDOVNO IZVRŠAVAJTE SVOJE OBVEZE\
+UČENJE NIJE MUČENJE\
+\
+Repetitio est mater studiorum ! (hrv. Ponavljanje je majka znanja !) \
+"/></div></div></div>');})();
